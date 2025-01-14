231243cc1f59b83045d5e21cbc014528079460ef
David Peterson <peterdav000@warren.k12.in.us>
2025-01-14 13:02:29 -0500

    11111

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int min = Integer.MAX_VALUE;
+
+        for (int number : numbers) {
+            min = Math.min(min, number);
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int number : numbers) {
+            max = Math.max(max, number);
+        }
+
+
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        double average = (double) sum / numbers.length;
+        System.out.println(average);
+
+
+
+    }
 }
